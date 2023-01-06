8ca1aaaf7a2b2db5a057c6086dddffd3db914c23
Katarzyna Baranska <katarzyna.kinga.baranska@gmail.com>
2023-01-06 23:40:36 +0100

    Dodanie logiki w klasie Main oraz pliku tekstowego "liczby.txt"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
 
 
     public static void main(String[] args) {
-
+        String fileName = "liczby.txt";
+        try {
+            List<Integer> allNumbers = getAllNumbers(fileName);
+            printInfo(allNumbers);
+        } catch (FileNotFoundException e) {
+            System.err.println("Brak pliku o nazwie: " + fileName);
+        }
     }
 
+    private static void printInfo(List<Integer> allNumbers) {
+        Set<Integer> numbersSet = getNumbersSet(allNumbers);
+        for (Integer uniqueNumber : numbersSet) {
+            int count = getCounterForUniqueNumber(allNumbers, uniqueNumber);
+            System.out.println(uniqueNumber + " - liczba wystąpień " + count);
+        }
+    }
+
+    private static int getCounterForUniqueNumber(List<Integer> allNumbers, Integer uniqueNumber) {
+        int count = 0;
+        for (Integer number : allNumbers) {
+            if (number.equals(uniqueNumber)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static List<Integer> getAllNumbers(String fileName) throws FileNotFoundException {
+        List<Integer> numbersList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                numbersList.add(scanner.nextInt());
+            }
+        }
+        return numbersList;
+    }
+
+    private static Set<Integer> getNumbersSet(List<Integer> allNumberList) {
+        Set<Integer> numbersSet = new HashSet<>();
+        for (Integer number : allNumberList) {
+            numbersSet.add(number);
+        }
+        return numbersSet;
+    }
 }
